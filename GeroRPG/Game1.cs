f617ae8def6d3695c63e8c28380436cdf22552fa
Geroshabu <oonstks@gmail.com>
2015-05-10 01:55:03 +0900

    Re-create MonoGame project for MonoGame3.4 with Visual Studio Express 2013

--- GeroRPG/Game1.cs
+++ GeroRPG/Game1.cs
@@ -1,18 +1,11 @@
-﻿#region Using Statements
-using System;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Storage;
-using Microsoft.Xna.Framework.GamerServices;
-#endregion
 
 namespace GeroRPG
 {
 	/// <summary>
-	/// This is the main type for your game
+	/// This is the main type for your game.
 	/// </summary>
 	public class Game1 : Game
 	{
@@ -20,7 +13,6 @@ namespace GeroRPG
 		SpriteBatch spriteBatch;
 
 		public Game1()
-			: base()
 		{
 			graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
@@ -53,7 +45,7 @@ namespace GeroRPG
 
 		/// <summary>
 		/// UnloadContent will be called once per game and is the place to unload
-		/// all content.
+		/// game-specific content.
 		/// </summary>
 		protected override void UnloadContent()
 		{
--- GeroRPG/Properties/AssemblyInfo.cs
+++ GeroRPG/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9f85029b-5ac2-40f1-a4fe-55e420c8bd0e")]
+[assembly: Guid("e98640d0-109d-4a4e-967f-09b32a9e8398")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.4.0.456")]
+[assembly: AssemblyFileVersion("3.4.0.456")]
